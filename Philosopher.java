d0adf72256f3a4526d19d5eee1e51b4cd96ac268
AbrahamMontalvo <amontalvo@carroll.edu>
2025-03-15 14:44:31 -0600

    Added Philosopher class

---
+++ Philosopher.java
@@ -0,0 +1,11 @@
+package DiningPhilosophers;
+
+public class Philosopher implements Runnable {
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            
+        }
+    }
+}
