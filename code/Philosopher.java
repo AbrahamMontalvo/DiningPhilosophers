281a3a43c3d8d70984f5e3bb704eda1ca64e82b1
AbrahamMontalvo <amontalvo@carroll.edu>
2025-04-13 18:37:10 -0600

    Formatted, added comments and javadocs

--- code/Philosopher.java
+++ code/Philosopher.java
@@ -1,17 +1,37 @@
 public class Philosopher implements Runnable {
-
     private final Thread thread;
+
+    // Shared between all instances, the total number of chopsticks on the table
+    public static int totalChopsticks;
+
+    // In each instance, the number of dumplings that a single Philosopher has eaten
     private int dumplingsEaten = 0;
-    private static int totalChopsticks;
+
+    // The number of chopsticks a single Philosopher currently has
     private int chopsticksOwned = 0;
-    private int threadNum;
+
+    // Used to distinguish each Philosopher instance
+    private final int threadNum;
+
+    // Used as the runner variable that stops the simulation when set to false
     public volatile boolean thinkAndEat;
+
+    // The number of Philosopher instances we want to create
     private static final int NUM_PHILOSOPHERS = 5;
+
+    // The length of the simulations we want to run
     public static final long PROCESSING_TIME = 5 * 1000;
+
+    // Boolean value that dictates whether or not it is the Philosopher's turn to
+    // eat
     private volatile boolean myTurn = false;
 
     /**
      * Method refactored by ChatGPT
+     * 
+     * Thread-running method that we use to control the behavior of each Philsopher
+     * instance
+     * Synchronized on the class itself to share the totalChopsticks variable
      */
     @Override
     public void run() {
@@ -29,6 +49,7 @@ public class Philosopher implements Runnable {
     /**
      * Method refactored by ChatGPT
      * 
+     * Allows the Philosopher to eat one dumpling before releasing the chopsticks
      */
     public void eat() {
         System.out.println("Philosopher #" + threadNum + " has started eating...");
@@ -38,6 +59,9 @@ public class Philosopher implements Runnable {
         release();
     }
 
+    /**
+     * Returns the chopsticks to be redistributed after eating a dumpling
+     */
     public void release() {
         totalChopsticks += 2;
         chopsticksOwned -= 2;
@@ -45,17 +69,27 @@ public class Philosopher implements Runnable {
         System.out.println("There is (are) " + totalChopsticks + " chopstick(s) left.\n");
     }
 
+    /**
+     * Constructor for our Philosopher classes with uniform chopstick needs (2)
+     * 
+     * @param i - The unique identifier and thread number
+     */
     public Philosopher(int i) {
         thread = new Thread(this, "Philosopher #" + i);
         this.threadNum = i;
     }
 
+    /**
+     * Starts the thread associated with the Philosopher
+     */
     public void startPhilosopher() {
         System.out.println("Philosopher #" + threadNum + " is active.");
         thread.start();
     }
 
     /**
+     * Method taken from JuiceBottler, thanks Nate
+     * 
      * @param time   - length of time we want to sleep the thread
      * @param errMsg - error message thrown when we slepe the thread and it
      *               throws the InterruptedException
@@ -70,14 +104,26 @@ public class Philosopher implements Runnable {
         }
     }
 
+    /**
+     * Sets the runner boolean to false and kills the thread
+     */
     public void stopPhilosopher() {
         thinkAndEat = false;
     }
 
+    /**
+     * Getter for number of chopsticks
+     * 
+     * @return The number of chopsticks that this Philosopher has
+     */
     public int getChopsticks() {
         return this.chopsticksOwned;
     }
 
+    /**
+     * Allows the philosopher to get a turn eating if there are enough chopsticks
+     * for them to eat
+     */
     public void acquire() {
         if (totalChopsticks >= 2) {
             myTurn = true;
@@ -87,6 +133,12 @@ public class Philosopher implements Runnable {
         }
     }
 
+    /**
+     * Function used to summarize each simulation
+     * 
+     * @return String value that summarizes the Philosopher's ability to eat
+     *         throughout the simulation
+     */
     public String getEaten() {
         return "Philosopher #" + threadNum + " ate " + dumplingsEaten + " dumplings";
     }
@@ -102,9 +154,18 @@ public class Philosopher implements Runnable {
         }
     }
 
+    /**
+     * Main method the initializes PhilosopherRandom instances and runs the
+     * simulation based on the parameters defined in the class
+     * 
+     * @param args - None taken
+     */
     public static void main(String[] args) {
         Philosopher[] philosophers = new Philosopher[NUM_PHILOSOPHERS];
-        totalChopsticks = NUM_PHILOSOPHERS;
+
+        // Only create full sets of chopsticks for distribution
+        totalChopsticks = ((int) ((NUM_PHILOSOPHERS / 2))) * 2;
+
         for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
             philosophers[i] = new Philosopher(i + 1);
             philosophers[i].startPhilosopher();
--- code/PhilosopherRandom.java
+++ code/PhilosopherRandom.java
@@ -2,19 +2,42 @@ import java.util.Random;
 
 public class PhilosopherRandom implements Runnable {
     private final Thread thread;
+
+    // Shared between all instances, the total number of chopsticks on the table
     public static int totalChopsticks;
+
+    // In each instance, the number of dumplings that a single Philosopher has eaten
     private int dumplingsEaten = 0;
+
+    // The number of chopsticks a single Philosopher currently has
     private int chopsticksOwned = 0;
+
+    // For the randomness simulation, the number of chopsticks a Philosopher needs
+    // to eat
     private final int sticksNeeded;
+
+    // Used to distinguish each Philosopher instance
     private final int threadNum;
+
+    // Used as the runner variable that stops the simulation when set to false
     public volatile boolean thinkAndEat;
+
+    // The number of Philosopher instances we want to create
     private static final int NUM_PHILOSOPHERS = 5;
+
+    // The length of the simulations we want to run
     public static final long PROCESSING_TIME = 5 * 1000;
+
+    // Boolean value that dictates whether or not it is the Philosopher's turn to
+    // eat
     private volatile boolean myTurn = false;
-    private volatile boolean starving = false;
 
     /**
      * Method refactored by ChatGPT
+     * 
+     * Thread-running method that we use to control the behavior of each Philsopher
+     * instance
+     * Synchronized on the class itself to share the totalChopsticks variable
      */
     @Override
     public void run() {
@@ -32,15 +55,20 @@ public class PhilosopherRandom implements Runnable {
     /**
      * Method refactored by ChatGPT
      * 
+     * Allows the Philosopher to eat one dumpling before releasing the chopsticks
      */
     public void eat() {
-        System.out.println("Philosopher #" + threadNum + " who needs " + sticksNeeded + " chopsticks to eat, has started eating.");
+        System.out.println(
+                "Philosopher #" + threadNum + " who needs " + sticksNeeded + " chopsticks to eat, has started eating.");
         System.out.println("There is (are) " + totalChopsticks + " chopstick(s) left.\n");
         dumplingsEaten += 1;
-        delay(500, "Eating interrupted"); // simulate eating
+        delay(500, "Eating interrupted"); // Simulate eating
         release();
     }
 
+    /**
+     * Returns the chopsticks to be redistributed after eating a dumpling
+     */
     public void release() {
         totalChopsticks += chopsticksOwned;
         chopsticksOwned = 0;
@@ -48,18 +76,29 @@ public class PhilosopherRandom implements Runnable {
         System.out.println("There is (are) " + totalChopsticks + " chopstick(s) left.\n");
     }
 
+    /**
+     * Constructor for our Philosopher classes with randomized chopstick needs
+     * 
+     * @param i     - The unique identifier and thread number
+     * @param stick - The number of chopsticks required to eat
+     */
     public PhilosopherRandom(int i, int stick) {
         thread = new Thread(this, "Philosopher #" + i);
         this.threadNum = i;
         this.sticksNeeded = stick;
     }
 
+    /**
+     * Starts the thread associated with the Philosopher
+     */
     public void startPhilosopher() {
         System.out.println("Philosopher #" + threadNum + " is active.");
         thread.start();
     }
 
     /**
+     * Taken from the JuiceBottler lab, thanks Nate
+     * 
      * @param time   - length of time we want to sleep the thread
      * @param errMsg - error message thrown when we slepe the thread and it
      *               throws the InterruptedException
@@ -74,14 +113,26 @@ public class PhilosopherRandom implements Runnable {
         }
     }
 
+    /**
+     * Sets the runner boolean to false and kills the thread
+     */
     public void stopPhilosopher() {
         thinkAndEat = false;
     }
 
+    /**
+     * Getter for number of chopsticks
+     * 
+     * @return The number of chopsticks that this Philosopher has
+     */
     public int getChopsticks() {
         return this.chopsticksOwned;
     }
 
+    /**
+     * Allows the philosopher to get a turn eating if there are enough chopsticks
+     * for them to eat
+     */
     public void acquire() {
         if (totalChopsticks >= sticksNeeded) {
             myTurn = true;
@@ -91,12 +142,18 @@ public class PhilosopherRandom implements Runnable {
         }
     }
 
+    /**
+     * Function used to summarize each simulation
+     * 
+     * @return String value that summarizes the Philosopher's ability to eat
+     *         throughout the simulation
+     */
     public String getEaten() {
         return "Philosopher #" + threadNum + " ate " + dumplingsEaten + " dumplings";
     }
 
     /**
-     * Waits for Philosopher to stop
+     * Waits for Philosopher to stop by using the join() method on the thread
      */
     public void waitToStop() {
         try {
@@ -106,12 +163,18 @@ public class PhilosopherRandom implements Runnable {
         }
     }
 
+    /**
+     * Main method the initializes PhilosopherRandom instances and runs the
+     * simulation based on the parameters defined in the class
+     * 
+     * @param args - None taken
+     */
     public static void main(String[] args) {
-        Random randNum= new Random();
+        Random randNum = new Random();
         PhilosopherRandom[] philosophers = new PhilosopherRandom[NUM_PHILOSOPHERS];
         totalChopsticks = NUM_PHILOSOPHERS;
         for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-            philosophers[i] = new PhilosopherRandom(i + 1, randNum.nextInt(1,totalChopsticks+1));
+            philosophers[i] = new PhilosopherRandom(i + 1, randNum.nextInt(1, totalChopsticks + 1));
             philosophers[i].startPhilosopher();
         }
 
